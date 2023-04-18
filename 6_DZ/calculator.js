4b79552612d3beb649ce5eef68d0d1134ad86069
ApiiiG <apcag.hrvoje@gmail.com>
2023-04-18 17:06:11 +0200

    6_DZ fixed Ajax

--- 6_DZ/calculator.js
+++ 6_DZ/calculator.js
@@ -2,44 +2,41 @@ const express = require('express');
 const fetch = require('node-fetch');
 const app = express();
 
-app.use(express.urlencoded({ extended: true }));
+
 app.use(express.static(__dirname + '/public'));
 
 app.get('/', (req, res) => {
   res.sendFile(__dirname + '/index.html');
 });
 
-app.post('/', (req, res) => {
-  const height = parseFloat(req.body.height);
-  const weight = parseFloat(req.body.weight);
+app.post('/calculate', async (req, res) => {
+  try {
+    // Fetch two random meals from MealDB API
+    const response = await fetch('www.themealdb.com/api/json/v1/1/randomselection.php');
+    const json = await response.json();
+    const meal1 = json.meals[0];
+    const meal2 = json.meals[1];
 
-  if (isNaN(height) || isNaN(weight) || height <= 0 || weight <= 0) {
-    res.status(400).send('Invalid input. Please enter valid values for height and weight.');
-  } else {
-    const bmi = calculateBMI(height, weight);
-    fetch('https://www.themealdb.com/api/json/v1/1/random.php')
-      .then(response => response.json())
-      .then(data => {
-        const meal = { name: data.meas[0].strMeal, thumbnail: data.meal[0].strMealThumb };
-        res.send(`
-          <h1>BMI Calculator</h1>
-          <p>Your BMI is ${bmi.toFixed(1)}</p>
-          <h2>Meal Recommendation:</h2>
-          <p>${meal.name}</p>
-          <img src="${meal.thumbnail}" alt="${meal.name}">
-        `);
-      })
-      .catch(error => {
-        console.log(error);
-        res.status(500).send('An error occurred while fetching data from the Meal DB API.');
-      });
+    // Extract name and thumbnail of meals
+    const meal1Name = meal1.strMeal;
+    const meal1Thumbnail = meal1.strMealThumb;
+    const meal2Name = meal2.strMeal;
+    const meal2Thumbnail = meal2.strMealThumb;
+
+    // Send data to client-side JavaScript
+    res.send({
+      meal1Name,
+      meal1Thumbnail,
+      meal2Name,
+      meal2Thumbnail,
+    });
+  } catch (error) {
+    console.error(error);
+    res.status(500).send('Error fetching meals');
   }
 });
 
-app.listen(3000, () => {
-  console.log('Server is listening on port 3000');
-});
 
-function calculateBMI(height, weight) {
-  return weight / ((height / 100) ** 2);
-}
+app.listen(3000, () => {
+  console.log('Server listening on port 3000');
+});
